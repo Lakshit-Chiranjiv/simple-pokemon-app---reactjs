448cbf6e690fe0b3d48a80ded0ac240c0047efdf
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-03-21 21:27:08 +0530

    app component setup for every page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,20 @@
+import react from "react";
+import { Container, Typography,Box,Button,IconButton } from '@mui/material';
+import AddCircleOutlineOutlinedIcon from '@mui/icons-material/AddCircleOutlineOutlined';
+
 function App() {
 
   return (
-    <div className="App">
-    </div>
+    <>
+      <Container sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', gap: 2,mt: 5 }}>
+        <Typography variant="h3" gutterBottom fontWeight='fontWeightBold' textAlign='center' color="initial">Pokemons Collection App</Typography>
+
+        <Box sx={{ p: 2, borderRadius: '50%',height: '40px', width: '40px',display: 'flex', justifyContent: 'center', alignItems: 'center',mb: 2 }}>
+          <IconButton variant="text"><AddCircleOutlineOutlinedIcon sx={{ fontSize: 60,color: 'green' }}/></IconButton>
+        </Box>
+      </Container>
+      <hr style={{width: '20%', backgroundColor: 'gray', height: '0.5px'}}/>
+    </>
   )
 }
 
